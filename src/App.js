424777bb47ff72f283fe0a61a67ad36318583b64
Rahul <rahul16086@gmail.com>
2021-07-04 15:59:40 +0530

    #2 Added auth page using spotify and obtained access token

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Login from "./components/Login/Login";
+import { useEffect, useState } from "react";
+import { getAccessTokenFromUrl } from "./spotify";
 
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getAccessTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+
+    if (token) setAccessToken(token);
+    console.log("Token here: ", accessToken);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Spotify clone !!</h1>
+    <div className="app">
+      {accessToken ? <h1>Hey, you are logged in :)</h1> : <Login />}
     </div>
   );
 }
