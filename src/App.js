d3a3a30a83a14e925bf03237837f59a8ae6815cb
Rahul <rahul16086@gmail.com>
2021-07-05 10:49:35 +0530

    #3 Obtained AccessToken from Url and added skeleton player

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import { useEffect, useState } from "react";
 import { getAccessTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [accessToken, setAccessToken] = useState(null);
@@ -11,7 +14,14 @@ function App() {
     window.location.hash = "";
     const token = hash.access_token;
 
-    if (token) setAccessToken(token);
+    if (token) {
+      setAccessToken(token);
+      spotify.setAccessToken(token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
     console.log("Token here: ", accessToken);
   }, []);
 
